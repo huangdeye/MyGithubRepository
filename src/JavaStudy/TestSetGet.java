7fd402a0ede1cc82fbadb46ceeac4f9855888a7e
huangdeye <deye_h@163.com>
2017-07-21 21:28:09 +0800

    sysout

--- src/JavaStudy/TestSetGet.java
+++ src/JavaStudy/TestSetGet.java
@@ -1,5 +1,13 @@
 package JavaStudy;
 
-public class TestSetGet {
+import org.omg.Messaging.SyncScopeHelper;
 
+public class TestSetGet {
+     
+	
+	public static void main(String[] args)
+	{
+		System.out.println();
+	}
+	
 }
